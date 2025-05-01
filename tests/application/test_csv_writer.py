c9839892c78b4801801f7a5531d6a80e486cb257
Nikolaj Andersen <nikolajandersen@its.aau.dk>
2025-05-01 13:18:35 +0200

    GitHub actions workflow debugging.

--- tests/application/test_csv_writer.py
+++ tests/application/test_csv_writer.py
@@ -1,49 +1,51 @@
 import json
 import os
+import unittest
+
 from whispaau.writers import WriteCSV
 from pathlib import Path
 
-current_path = Path(os.path.dirname(os.path.realpath(__file__)))
-test_file_path = current_path / "resources" / "test_output.csv"
-expected_file_path = current_path / "resources" / "expected_output_missing_speaker.csv"
-transcription_missing_speaker_path = current_path / "resources" / "shorts_missing_speaker_fist_line.json"
-transcription_with_bad_attribute_path = current_path / "resources" / "transcription_with_bad_attribute.json"
+class TestCsvWriter(unittest.TestCase):
+    def setUp(self):
+        current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+        self.test_file_path = current_path / "resources" / "test_output.csv"
+        self.expected_file_path = current_path / "resources" / "expected_output_missing_speaker.csv"
+        self.transcription_missing_speaker_path = current_path / "resources" / "shorts_missing_speaker_fist_line.json"
+        self.transcription_with_bad_attribute_path = current_path / "resources" / "transcription_with_bad_attribute.json"
 
-csv_writer = WriteCSV("")
+    def test_csv_writer(self):
+        csv_writer = WriteCSV("")
 
-test_file_name: str = 'application/resources/test_output.csv'
-expected_file_name: str = 'application/resources/expected_output_missing_speaker.csv'
+        # read JSON test file
+        with open(self.transcription_missing_speaker_path) as json_file:
+            data = json.load(json_file)
 
-# read JSON test file
-with open(transcription_missing_speaker_path) as json_file:
-    data = json.load(json_file)
+        file = open(self.test_file_path, 'w')
 
-file = open(test_file_path, 'w')
+        # write the CSV file
+        csv_writer.write_result(data, file, {})
 
-# write the CSV file
-csv_writer.write_result(data, file, {})
+        expected_output_file = open(self.expected_file_path)
 
-expected_output_file = open(expected_file_path)
+        # compare the generated file with the expected output
+        with open(self.test_file_path, 'r') as file:
+            output = file.read()
+            expected_output = expected_output_file.read()
+            assert output == expected_output, 'The generated output does not match the expected output.'
 
-# compare the generated file with the expected output
-with open(test_file_path, 'r') as file:
-    output = file.read()
-    expected_output = expected_output_file.read()
-    assert output == expected_output, 'The generated output does not match the expected output.'
+        # clean up after test, remove the generated test file
+        os.remove(self.test_file_path)
 
-# clean up after test, remove the generated test file
-os.remove(test_file_path)
+        # verify bugfix for https://github.com/aau-claaudia/transcriber/issues/20
+        csv_writer = WriteCSV("Test bugfix")
 
-# verify bugfix for https://github.com/aau-claaudia/transcriber/issues/20
-csv_writer = WriteCSV("Test bugfix")
+        # read JSON test file
+        with open(self.transcription_with_bad_attribute_path) as json_file:
+            data = json.load(json_file)
 
-# read JSON test file
-with open(transcription_with_bad_attribute_path) as json_file:
-    data = json.load(json_file)
+        file = open(self.test_file_path, 'w')
+        # write the CSV file (should not give an error with bugfix)
+        csv_writer.write_result(data, file, {})
 
-file = open(test_file_path, 'w')
-# write the CSV file (should not give an error with bugfix)
-csv_writer.write_result(data, file, {})
-
-# clean up after test, remove the generated test file
-os.remove(test_file_path)
+        # clean up after test, remove the generated test file
+        os.remove(self.test_file_path)
