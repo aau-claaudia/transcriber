e43c8f398914cb482203055266f36df60eee67b6
Nikolaj Andersen <nikolajandersen@its.aau.dk>
2025-05-01 12:46:34 +0200

    GitHub actions workflow debugging.

--- tests/application/test_speaker_grouping.py
+++ tests/application/test_speaker_grouping.py
@@ -1,10 +1,16 @@
 import json
+import os
 from whispaau.writers import merge_speakers
+from pathlib import Path
 
-with open('resources/transcription_with_same_speaker.json') as json_file:
+current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+transcription_with_same_speaker = current_path / "resources" / "transcription_with_same_speaker.json"
+expected_grouped_speakers = current_path / "resources" / "expected_grouped_speakers.json"
+
+with open(transcription_with_same_speaker) as json_file:
     data = json.load(json_file)
 
-with open('resources/expected_grouped_speakers.json') as json_file:
+with open(expected_grouped_speakers) as json_file:
     expected_data = json.load(json_file)
 
 assert merge_speakers(data) == expected_data
