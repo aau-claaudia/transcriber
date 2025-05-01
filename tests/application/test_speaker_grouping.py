5c2c39cc0cf789c436c5e1b0e04fc7b7b3a73045
Nikolaj Andersen <nikolajandersen@its.aau.dk>
2025-05-01 10:47:54 +0200

    GitHub actions workflow debugging.

--- tests/application/test_speaker_grouping.py
+++ tests/application/test_speaker_grouping.py
@@ -1,10 +1,10 @@
 import json
 from whispaau.writers import merge_speakers
 
-with open('../resources/transcription_with_same_speaker.json') as json_file:
+with open('resources/transcription_with_same_speaker.json') as json_file:
     data = json.load(json_file)
 
-with open('../resources/expected_grouped_speakers.json') as json_file:
+with open('resources/expected_grouped_speakers.json') as json_file:
     expected_data = json.load(json_file)
 
 assert merge_speakers(data) == expected_data
