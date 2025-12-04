fc5f6dc8c747d5029e0a43761455d2193fdfcb29
Nikolaj Andersen <nikolajandersen@its.aau.dk>
2025-12-04 14:13:11 +0100

    Trying workaround for whisperx issue: https://github.com/m-bain/whisperX/issues/1304

--- huggingface-download.sh
+++ huggingface-download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pip install -U "huggingface_hub[cli]"
-hf login --token [hftoken]
+hf auth login --token [hftoken]
 hf download pyannote/speaker-diarization-3.1 --cache-dir ~/.cache/torch/pyannote/
 hf download pyannote/segmentation-3.0 --cache-dir ~/.cache/torch/pyannote/
