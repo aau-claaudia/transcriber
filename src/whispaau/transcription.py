27cd4dca97c344b5c8c8a6bde62be2f7e176308c
Nikolaj Andersen <nikolajandersen@its.aau.dk>
2026-03-17 11:41:22 +0100

    WIP buffered transcription for long audio.

--- src/whispaau/transcription.py
+++ src/whispaau/transcription.py
@@ -1,9 +1,12 @@
+import os
+import tempfile
 from pathlib import Path
 from typing import List, TypedDict, Any, Dict, Union, Tuple
 from time import perf_counter_ns
 
 import numpy as np
 import librosa
+import soundfile as sf
 
 from whispaau.logging import Logger
 from abc import ABC, abstractmethod
@@ -43,6 +46,7 @@ class TranscriptionService(ABC):
 # A module-level cache to store instantiated transcriber services.
 # The key is a tuple (model_name, device).
 _model_cache: Dict[Union[str, Tuple[str, str]], "TranscriptionService"] = {}
+
 
 def transcribe(model_name: str, file: Path, trans_arguments: Any, device, log: Logger, duration: float):
     """
@@ -100,9 +104,15 @@ class ParakeetTranscription(TranscriptionService):
     def __init__(self, model_name: str, device, log: Logger, duration: float):
         self.log = log
         self.model_name = model_name
+        self.duration = duration
         start_time = perf_counter_ns()
         self.model = nemo_asr.models.ASRModel.from_pretrained(model_name="nvidia/parakeet-tdt-0.6b-v3", map_location=device)
         self.log.log_model_loading(self.model_name, start_time, perf_counter_ns())
+        self.sample_rate = 16000 # 16kHz
+        self.chunk_length_s: int = 30
+        self.overlap_s: int = 1
+        self.batch_size: int = 16
+        self.buffering_threshold: int = 300
         if duration > 30:
             log.get_logger().info("Audio longer than 30 seconds, using long form transcription.")
             # updating self-attention model of fast-conformer encoder
@@ -111,46 +121,121 @@ class ParakeetTranscription(TranscriptionService):
         self.model = self.model.to(device)
 
     def transcribe(self, file: Path, trans_arguments: Any) -> dict[str, Any]:
-        output = self.model.transcribe(convert_to_16k_mono_audio(file.resolve().as_posix()), timestamps=True)
+        audio = convert_to_16k_mono_audio(self.sample_rate, file.resolve().as_posix())
 
-        # Extract text and timestamps
-        result_data = output[0]
-        text = result_data.text
-        #word_timestamps = result_data.timestamp.get("word", [])
-        segment_timestamps = result_data.timestamp.get("segment", [])
-        #print(f"Transcription: {text}")
-        #print(f"Segment data: {segment_timestamps}")
+        if self.duration > 60: #TODO: change to: self.duration > self.buffering_threshold:
+            # if the audio is longer than 5 minutes transcribe in chunks to manage memory consumption
+            return self.transcribe_buffered(audio)
+        else:
+            output = self.model.transcribe(audio, timestamps=True)
+            # Extract text and timestamps
+            result_data = output[0]
+            text = result_data.text
+            #word_timestamps = result_data.timestamp.get("word", [])
+            segment_timestamps = result_data.timestamp.get("segment", [])
+            # Prepare output data
+            result = prepare_output(segment_timestamps, text)
+            return result
 
-        # Prepare output data
-        segments: List[TranscriptionSegment] = []
-        for i, item in enumerate(segment_timestamps, start=1):
-            # create the TranscriptionSegment, missing fields are set to defaults
-            seg: TranscriptionSegment = {
-                "id": i,
-                "seek": 0,
-                "start": item["start"],
-                "end": item["end"],
-                "text": item["segment"],
-                "tokens": [],
-                "temperature": 0.0,
-                "avg_logprob": 0.0,
-                "compression_ratio": 0.0,
-                "no_speech_prob": 0.0
-            }
-            segments.append(seg)
-        result: TranscriptionResult = {
-            "text": text,
-            "segments": segments,
-            "language": "en"
+    def transcribe_buffered(self, audio) -> dict[str, Any]:
+        # Calculate chunk and overlap lengths in samples
+        chunk_len_samples = self.chunk_length_s * self.sample_rate
+        overlap_samples = 8000 # = self.overlap_s * self.sample_rate # TODO: experimenting with half a second
+        step = chunk_len_samples - overlap_samples
+
+        # Create a temporary directory for chunks
+        with tempfile.TemporaryDirectory() as temp_dir:
+            chunks = []
+            start = 0
+            idx = 0
+            while start < len(audio):
+                end = start + chunk_len_samples
+                chunk = audio[start:end]
+
+                chunk_filepath = os.path.join(temp_dir, f"chunk_{idx}.wav")
+                sf.write(chunk_filepath, chunk, self.sample_rate)
+
+                chunks.append({
+                    'audio_path': chunk_filepath,
+                    'start_time': start / self.sample_rate,
+                    'duration': len(chunk) / self.sample_rate
+                })
+
+                start += step
+                idx += 1
+            self.log.get_logger().info(f"Audio was split into {len(chunks)} chunks.")
+
+            # Transcribe all chunks
+            self.log.get_logger().info(f"Transcribing chunks...")
+            #all_words = []
+            all_segments = []
+            full_text: str = ""
+            for i, chunk_info in enumerate(chunks):
+                self.log.get_logger().info(f"Transcribing chunk {i + 1}/{len(chunks)} (duration: {chunk_info['duration']:.1f}s)...")
+                # Transcribe chunk
+                output = self.model.transcribe(
+                    chunk_info['audio_path'],
+                    batch_size=1,
+                    timestamps=True,
+                )
+
+                result_data = output[0]
+                chunk_text = result_data.text
+                full_text.join(chunk_text)
+
+                # Extract and adjust timestamps
+                if hasattr(result_data, 'timestamp') and result_data.timestamp:
+                    chunk_words = result_data.timestamp.get("word", [])
+                    chunk_segments = result_data.timestamp.get("segment", [])
+
+                    # Adjust timestamps by chunk start time
+                    #for word in chunk_words:
+                    #    word['start'] += chunk_info['start_time']
+                    #    word['end'] += chunk_info['start_time']
+                    #    all_words.append(word)
+
+                    for segment in chunk_segments:
+                        segment['start'] += chunk_info['start_time']
+                        segment['end'] += chunk_info['start_time']
+                        all_segments.append(segment)
+
+                self.log.get_logger().info(f"Chunk {i + 1} complete: {len(chunk_text)} characters")
+
+            # Prepare output data
+            result = prepare_output(all_segments, full_text)
+            return result
+
+
+def prepare_output(all_segments, full_text):
+    segments: List[TranscriptionSegment] = []
+    for i, item in enumerate(all_segments, start=1):
+        # create the TranscriptionSegment, missing fields are set to defaults
+        seg: TranscriptionSegment = {
+            "id": i,
+            "seek": 0,
+            "start": item["start"],
+            "end": item["end"],
+            "text": item["segment"],
+            "tokens": [],
+            "temperature": 0.0,
+            "avg_logprob": 0.0,
+            "compression_ratio": 0.0,
+            "no_speech_prob": 0.0
         }
-        return result
+        segments.append(seg)
+    result: TranscriptionResult = {
+        "text": full_text,
+        "segments": segments,
+        "language": "en"
+    }
+    return result
 
 
 """Convert audio to mono 16kHz"""
-def convert_to_16k_mono_audio(file: str) -> np.ndarray:
+def convert_to_16k_mono_audio(sample_rate, file: str) -> np.ndarray:
     audio_arr, sr = librosa.load(file, sr=None)
     if len(audio_arr.shape) > 1:
         audio_arr = librosa.to_mono(audio_arr)
-    if sr != 16000:
-        audio_arr = librosa.resample(audio_arr, orig_sr=sr, target_sr=16000)
+    if sr != sample_rate:
+        audio_arr = librosa.resample(audio_arr, orig_sr=sr, target_sr=sample_rate)
     return audio_arr
